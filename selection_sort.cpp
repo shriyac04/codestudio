3e72d26a466a13b0184461f014b92491291d7c2d
shriyac04 <122461216+shriyac04@users.noreply.github.com>
2024-02-05 16:58:01 +0530

    selection sort

---
+++ selection_sort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+void selectionSort(vector<int>& arr, int n)
+{   for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+        if(arr[i]>arr[j]){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+}
+    // Write your code here.
+}
